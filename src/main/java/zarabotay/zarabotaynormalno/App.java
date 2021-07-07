835d370b4c34e91af271875b6130ef58ad4a95e4
Angelina Denisova <den.angelina2000@yandex.ru>
2021-07-08 00:39:42 +0400

    На всё воля случая, и он иногда счастливый.

--- src/main/java/zarabotay/zarabotaynormalno/App.java
+++ src/main/java/zarabotay/zarabotaynormalno/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Всем спокойной ночи, пора спать!" );
+        System.out.println( "На всё воля случая" );
     }
 }
