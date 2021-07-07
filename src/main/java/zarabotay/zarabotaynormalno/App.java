e4e8e79e82a4f83023def97f24c074efe5d97e37
Angelina Denisova <den.angelina2000@yandex.ru>
2021-07-07 23:36:29 +0400

    first commit

---
+++ src/main/java/zarabotay/zarabotaynormalno/App.java
@@ -0,0 +1,13 @@
+package zarabotay.zarabotaynormalno;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
